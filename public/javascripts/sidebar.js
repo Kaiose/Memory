a37e8f8ba41cb3b3de879e982f60d5622fa829e3
Kaiose <34887723+Kaiose@users.noreply.github.com>
2022-12-10 17:45:08 +0900

    sidebar type change
    change type link to button

---
+++ public/javascripts/sidebar.js
@@ -0,0 +1,7 @@
+var bar_items = document.querySelectorAll('.bar_item');
+
+bar_items.forEach(bar_item => bar_item.addEventListener('click', () => {
+  var title = bar_item.getAttribute('title');
+  console.log(title);
+  window.history.pushState('param', 'unused', `/home/${title}`);
+}));
